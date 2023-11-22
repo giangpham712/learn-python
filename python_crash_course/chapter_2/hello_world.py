82ff062e663e9cdda493a8da4d40395c0123097f
Giang Pham <giang.phamhoang712@gmail.com>
2023-11-23 05:11:55 +1100

    Add code for chapter 1 and 2

---
+++ python_crash_course/chapter_2/hello_world.py
@@ -0,0 +1,13 @@
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message.upper())
+
+first_name = "Giang"
+last_name = "Pham"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+nostarch_url = "https://nostarch.com"
+print(nostarch_url.removeprefix("https://"))
